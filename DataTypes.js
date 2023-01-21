40c8251409340c5fb3e4fdd913062ac139c5c10d
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-21 23:23:38 +0600

    types of number

---
+++ DataTypes.js
@@ -0,0 +1,11 @@
+var string="mozaihd"
+console.log(typeof(string))
+
+var number=3434
+console.log(typeof(number))
+
+var undefined=
+console.log(typeof(undefined))
+
+var name=null
+console.log(name)
