6c4df11225974f5356cc950c125b890fdc73d081
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-22 15:57:53 +0600

    sum

---
+++ Sum.js
@@ -0,0 +1,53 @@
+// 01.Arithmentic
+// + - / %  ++ -- 
+var a=80;
+var b=20;
+var sum =a+b;
+console.log(sum)
+
+
+// 02.Comparison
+//  ==  ===  != < > <= >=
+// var name1=12
+// var name2=12
+// var result= name1==name2
+// console.log(result)
+
+// var name1=12
+// var name2='12'
+// var result= name1===name2
+// console.log(result)
+
+
+// var name1=12
+// var name2=16
+// var result= name1 != name2
+// console.log(result)
+
+// var name1=12
+// var name2=16
+// var result= name1 < name2
+// console.log(result)
+
+// var name1=12
+// var name2=12
+// var result= name1 <= name2
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
