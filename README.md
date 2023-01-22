4508c9809db4f231a468e022460012945163a90f
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-22 16:11:08 +0600

    ++

--- README.md
+++
@@ -1,1 +0,0 @@
-<h4>Flow This Blogs : https://medium.com/@mozahidwebmin11d10/basic-to-advance-javascript-36ea8e0d9c86</h4>
--- Sum.js
+++ Sum.js
@@ -8,36 +8,56 @@ console.log(sum)
 
 // 02.Comparison
 //  ==  ===  != < > <= >=
-// var name1=12
-// var name2=12
-// var result= name1==name2
-// console.log(result)
+var name1=12
+var name2=12
+var result= name1==name2
+console.log(result)
 
-// var name1=12
-// var name2='12'
-// var result= name1===name2
-// console.log(result)
-
-
-// var name1=12
-// var name2=16
-// var result= name1 != name2
-// console.log(result)
-
-// var name1=12
-// var name2=16
-// var result= name1 < name2
-// console.log(result)
-
-// var name1=12
-// var name2=12
-// var result= name1 <= name2
-// console.log(result)
+var name1=12
+var name2='12'
+var result= name1===name2
+console.log(result)
 
 
+var name1=12
+var name2=16
+var result= name1 != name2
+console.log(result)
+
+var name1=12
+var name2=16
+var result= name1 < name2
+console.log(result)
+
+var name1=12
+var name2=0
+var result= name1 <= name2
+console.log('hdfhdfh',result)
 
 
+// 03.Logical 
+// && 2 tay true hte hbe
+// || jekono akta true hte hbe
+// ! man oltiye fela
 
+
+// Assignment :
+// += -= *= /= %=
+var a=10
+var result=a+10
+console.log(result)
+
+var a=10
+a -=10
+console.log(a)
+
+
+// 05.Conditional /Ternary
+// ?    : 
+var a=200
+var b=30
+var c =a>b? 'hoyche':'hoyni'
+console.log(c)
 
 
 
