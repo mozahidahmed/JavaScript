27a377e33ef5299836e15737baba62834accec66
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-23 15:21:56 +0600

    condition add

---
+++ ConditionalState.js
@@ -0,0 +1,46 @@
+// var number=-4;
+// if (number>40){
+//     console.log('success: 40 teke boro')
+
+// }
+// else if (number <= 0){
+//     console.log('success: 0000')
+
+// }
+// else if (number<40){
+//     console.log('success: 40 teke shoto')
+
+// }
+
+// else{
+//     console.log(Hoyni)  
+// }
+
+// var num=10
+// if(num>1){
+
+//     if(num>10){
+//          console.log('success')
+//      }else{
+//         console.log('1 teke boro na tai function chalai ni')
+//     }
+// }
+
+
+// Switch Case condition
+// var bar='Monday'
+// switch(bar){
+//     case 'Saturday':
+//         console.log(bar)
+//         break
+//     case 'Sunday':
+//         console.log(bar)
+//         break
+//     case 'monday':
+//         console.log(bar)
+//         break
+
+// default:
+//     console.log('no day ')  
+        
+// }
