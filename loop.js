993e536b5f0c9ab3e498160088d346ae5f59f6b9
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-01-24 11:19:29 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,38 @@
+
+// For Loop
+for (let i = 0; i < 10; i++) { 
+   console.log(i)
+    
+}
+for (let i = 0; i < 10; i++) {
+    if(i===5){
+        console.log('ami',i)
+        break
+    }
+   console.log(i)
+    
+}
+for (let i = 0; i < 10; i++) {
+    if(i===5){
+        console.log('ami',i)
+        continue
+    }
+   console.log(i)
+    
+}
+
+
+
+// Do while loop
+var number=0;
+do {
+    console.log(number)
+    number++
+
+}while(number < 10)
+
+
+
+
+
+
